59b131a56dd1e9e67e2fe78e35694d71f723f5e5
jordanjw404 <jordannjww@gmail.com>
2025-06-17 11:41:57 +0100

    Add initial structure for users, orders, inventory, factory_kpi, and production apps

--- factory_system/settings.py
+++ factory_system/settings.py
@@ -32,6 +32,12 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'core',
+    'users',
+    'customers',
+    'orders',
+    'inventory',
+    'factory_kpi',
+    'sales_order',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
